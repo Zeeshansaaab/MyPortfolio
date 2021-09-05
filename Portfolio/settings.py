9da58350d93ed39c5d6b86b9655e9cd13034abd6
Abdullah-rashed <Zeeshan@DESKTOP-US7F924>
2021-09-05 14:44:07 +0500

    twentysixth Commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static')
 ]
